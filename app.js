df640b63f7e3761db16485f157fcca34a7ec3859
OksanaKotsur <oxanaua1@gmail.com>
2022-02-15 22:27:34 +0200

    lesson1_hw_start

---
+++ app.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const fs = require('fs');
+
+// fs.mkdir(path.join(__dirname, 'lesson1_hw'), (err) => {
+//     if (err) {
+//         console.log(err);
+//         throw err
+//
+//     }
+// });
+// fs.mkdir(path.join(__dirname, 'lesson1_hw', 'main', 'online'), {recursive: true}, (err) => {
+//     if (err) {
+//         console.log(err);
+//         throw err
+//
+//     }
+// });
+// fs.mkdir(path.join(__dirname, 'lesson1_hw', 'main', 'inPerson'), {recursive: true}, (err) => {
+//     if (err) {
+//         console.log(err);
+//         throw err
+//
+//     }
+// });
+// fs.appendFile(path.join(__dirname, 'app.js'), 'const onlineUsers=[{\nname: "Andrii", \nage: 22, \ncity: "Lviv" }];', (err) => {
+//     if (err) {
+//         console.log(err);
+//         throw err
+//
+//     }
+// });
+// const onlineUsers = [{
+//     name: "Andrii",
+//     age: 22,
+//     city: "Lviv"
+// }];
+// fs.appendFile(path.join(__dirname, 'app.js'), 'const inPersonUsers=[{\nname: "Andrii", \nage: 22, \ncity: "Lviv" }];', (err) => {
+//     if (err) {
+//         console.log(err);
+//         throw err
+//
+//     }
+// });
+// const inPersonUsers = [{
+//     name: "Andrii",
+//     age: 22,
+//     city: "Lviv"
+// }];
+
