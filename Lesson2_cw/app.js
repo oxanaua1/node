25aa6921d85fbf6bba77f5110f15ce85c2a796e4
OksanaKotsur <oxanaua1@gmail.com>
2022-02-23 01:47:34 +0200

    lesson2_cw

---
+++ Lesson2_cw/app.js
@@ -0,0 +1,108 @@
+//// Необхідно розширити ваше ДЗ:
+////- додайте ендпоінт signIn, який буде приймати email і password і якщо все вірно то редірект на сторінку цього юзера
+
+//// * хто хоче складніше реалізуйте видалення користувача. Кнопка повинна знаходитись на сторінці з інфою про
+//// одного юзера.
+////Після видалення редірект на "/users"
+
+const express = require('express');
+const {engine} = require('express-handlebars');
+const path = require('path');
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'static')));
+
+app.set('view engine', '.hbs');
+app.engine('.hbs', engine({defaultLayout: false}));
+app.set('views', path.join(__dirname, 'static'));
+
+let users = [];
+let error = '';
+
+app.get('/signIn', (req, res) => {
+    res.render('signIn');
+});
+
+app.post('/signIn', ({ body }, res) => {
+    const user = users.find(user => user.email === body.email && user.password === body.password);
+
+    if (!user) {
+        error = 'Wrong email or password!';
+        res.redirect('/error');
+        return;
+    }
+
+    res.redirect(`/users/${user.id}`);
+});
+
+
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
+app.post('/login', ({body}, res) => {
+    const userExist = users.some(user => user.email === body.email);
+
+    if (userExist) {
+        error = 'User with this email already exists';
+        res.redirect('/error');
+        return;
+    }
+
+    users.push({...body, id: users.length ? users[users.length - 1].id + 1 : 1})
+    res.redirect('/users');
+
+});
+
+app.get('/users', ({query}, res) => {
+    if (Object.keys(query).length) {
+        let usersArray = [...users];
+
+        if (query.city) {
+            usersArray = usersArray.filter(user => user.city === query.city);
+        }
+        if (query.age) {
+            usersArray = usersArray.filter(user => user.age === query.age);
+        }
+
+        res.render('users', {users: usersArray});
+        return;
+    }
+
+    res.render('users', {users});
+});
+
+app.get('/users/:userId', ({params}, res) => {
+    const user = users.find(user => user.id === +params.userId);
+
+    if (!user) {
+        error = `User with ID: ${params.userId} does not exist!`;
+        res.redirect('/error');
+        return;
+    }
+
+    res.render('userInfo', {user});
+});
+
+app.post('/users/:userId', ({params}, res) => {
+    users = users.filter(user => user.id !== +params.userId)
+
+    res.redirect('/users')
+});
+
+app.get('/error', (req, res) => {
+    res.render('error', {error});
+});
+
+app.use((req, res) => {
+    res.render('notFound');
+});
+
+
+app.listen(5200, () => {
+    console.log('Server has started on PORT 5200');
+});
+
+
